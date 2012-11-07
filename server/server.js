25ec9074d6ccf0b3de5173ba6e24121a3b4c4e28
Christopher Mitchell <chris@chrismitchell.name>
2012-11-06 21:31:48 -0800

    server now watches files that are siblings to requested path
    At least for me (inotify in Fedora 16), fs.watch on a directory triggers change
    events when files within the directory are created or updated (deletes are
    renames).

--- server/server.js
+++ server/server.js
@@ -1,24 +1,25 @@
 var http = require('http');
 var url = require('url');
+var path = require('path');
 var fs = require('fs');
 
 
-var server = http.createServer(function (req, res) {
+http.createServer(function (req, res) {
   // skip over favicon requests that'll happen while developing
   if (url.parse(req.url).pathname === '/favicon.ico') { return; }
-
-  // path will be from window.location.pathname
-  var path = url.parse(req.url, true).query['path'] || '/';
 
   res.setHeader('Content-Type', 'text/plain');
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.statusCode = 200;
 
-  console.log('Watching "' + path + '"');
-  fs.watch(path, function (event, filename) {
-    console.log('Change detected.');
-    res.end(event);
-  });
-});
+  var pathname = url.parse(req.url, true).query['path'] || '/';
+  var dirname = path.dirname(pathname);
 
-server.listen(7053, '127.0.0.1');
+  console.log('Watching "' + dirname + '"');
+  fs.watch(dirname, function (event, filename) {
+    console.log('Change detected.', event, filename);
+    if (event === 'change') {
+      res.end(event);
+    }
+  });
+}).listen(7053, '127.0.0.1');
